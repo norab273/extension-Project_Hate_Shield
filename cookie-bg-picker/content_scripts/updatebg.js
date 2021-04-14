dae211a227dee88ee0ccc6255f844b6897fbc2d5
Juliette Baron <juliette.baron.33@gmail.com>
2021-04-14 19:51:13 +0200

    avec la modification du curseur et du background - échec pour la modification de la scrollbar

---
+++ cookie-bg-picker/content_scripts/updatebg.js
@@ -0,0 +1,22 @@
+// reçoit message de background ici la valeur image et color
+browser.runtime.onMessage.addListener(updateBg);
+
+function updateBg(request, sender, sendResponse) {
+  var html = document.querySelector("html");
+  var body = document.querySelector("body");
+  if (request.image) {
+    html.style.cursor = "url(" + request.image + "), auto";
+    body.style.cursor = "url(" + request.image + "),, auto";
+  } else if (request.tab) {
+    html.style.backgroundImage = "url(" + request.tab + ")";
+    body.style.backgroundImage = "url(" + request.tab + ")";
+  } else if (request.color) {
+    html.style.backgroundColor = request.color;
+    body.style.backgroundColor = request.color;
+  } else if (request.reset) {
+    html.style.backgroundImage = "";
+    html.style.backgroundColor = "";
+    body.style.backgroundImage = "";
+    body.style.backgroundColor = "";
+  }
+}
